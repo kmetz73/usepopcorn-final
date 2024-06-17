1b9ca12c452cf5f1d97ab3f54e972dc6342e08f0
kmetz73 <kmetz73@gmail.com>
2024-06-17 13:07:52 -0500

    useRef

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,19 +3,22 @@ import { useEffect, useRef } from "react";
 export default function Search({ query, setQuery }) {
   const inputEl = useRef(null);
 
-  useEffect(function () {
-    function callback(e) {
-      if (document.activeElement === inputEl.current) return;
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
 
-      if (e.key === "Enter") {
-        inputEl.current.focus();
-        setQuery("");
+        if (e.key === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
       }
-    }
 
-    document.addEventListener("keydown", callback);
-    return () => document.addEventListener("keydown", callback);
-  }, []);
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
 
   // useEffect(function () {
   //   const el = document.querySelector(".search");
